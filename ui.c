787844a8fab754341d0f97d92c21ef42cf98109f
kthye <studentkalvin.t@gmail.com>
2016-11-01 21:19:13 -0400

    Update to ui file to have proper coordinates

--- ui.c
+++ ui.c
@@ -1,5 +1,8 @@
 #include "ui.h"
 #include <stdlib.h>
+#include <stdio.h>
+#include "track.h"
+
 #define TILE_WIDTH 10
 #define TILE_WIDTH_BORDER 1
 #define TILE_HEIGHT 21
@@ -8,7 +11,7 @@
 #define BARRICADE_HEIGHT 19
 #define BARRICADE_BUFFER_X (TILE_WIDTH-BARRICADE_WIDTH)/2
 #define BARRICADE_BUFFER_Y (TILE_HEIGHT-BARRICADE_HEIGHT)/2
-#define SCREEN_HEIGHT 128
+#define SCREEN_HEIGHT 127
 #define SCREEN_WIDTH 32
 
 int screenVerticalLimit (int y) {
@@ -19,31 +22,48 @@ int screenVerticalLimit (int y) {
   return y;
 }
 
+void OrbitOledMoveTo(int a, int b) {
+  printf("Move Cursor to:(%d, %d)\n", a, b);
+}
+
+void OrbitOledFillRect(int a, int b) {
+  printf("Draw to: (%d, %d)\n", a, b);
+}
+
 void draw(tile *onScreen, int offset) {
   int row = 0;
   int tileX, tileY;
   int shift = TILE_HEIGHT - offset;
 
   ///iterates through on screen elements and draws them on screen
-  for (tile *i = onScreen; i != NULL; i = i->next) {
+  for (tile *i = onScreen; i != NULL && row < 7; i = i->next) {
+    if ((SCREEN_HEIGHT - row * TILE_HEIGHT - TILE_HEIGHT_BORDER) < 0)
+    break;
     for (int column = 0; column < sizeof(i->value)/sizeof(i->value[0]); column++) {
-      tileX = column * TILE_WIDTH + TILE_WIDTH_BORDER;
-      tileY = row * TILE_HEIGHT + TILE_HEIGHT_BORDER;
-      
+      tileX = SCREEN_HEIGHT - row * TILE_HEIGHT - TILE_HEIGHT_BORDER;
+      tileY = column * TILE_WIDTH + TILE_WIDTH_BORDER;
       if (offset != 0)
-        tileY -= shift;
-      tileY = screenVerticalLimit(tileY);
+        tileX += shift;
+      tileX = screenVerticalLimit(tileX);
       OrbitOledMoveTo(tileX, tileY);
       if (i->value[column]) {
-        OrbitOledMoveTo(tileX + BARRICADE_BUFFER_X, screenVerticalLimit(tileY + BARRICADE_BUFFER_Y));
-        OrbitOledFillRect(tileX + TILE_WIDTH - BARRICADE_BUFFER_X, screenVerticalLimit(tileY + TILE_HEIGHT - BARRICADE_BUFFER_Y));
+        OrbitOledMoveTo(screenVerticalLimit(tileX - BARRICADE_BUFFER_X), tileY + BARRICADE_BUFFER_Y);
+        OrbitOledFillRect(screenVerticalLimit(tileX - TILE_WIDTH + BARRICADE_BUFFER_X), tileY + TILE_HEIGHT - BARRICADE_BUFFER_Y);
       }
     }
     row++;
   }
 
-
-
+  //OrbitOledUpdate();
 }
 
+int main(void) {
+  tile *test = tileCreate();
+  test = trackPopTile(test);
+  test = trackPopTile(test);
+
+  for (tile *i = test; i != NULL; i = i->next) {
+      printf("%d\n", i->value[0]);
+  }
+}
 
