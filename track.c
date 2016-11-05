b8d03c7ad356fd5fbb729f6cf099c678495b47a9
Corbin McElhanney <corbin.mcelhanney@gmail.com>
2016-11-04 20:08:54 -0400

    Add exit-based barricade generation

--- track.c
+++ track.c
@@ -1,22 +1,95 @@
 #include "track.h"
 
+void setTileValue(bool val[NUMBER_OF_LANES]) {
+    // Requires that NUMBER_OF_LANES == 3
+    // Otherwise, changes to logic must be made
+    static bool isExit[NUMBER_OF_LANES] = {true, true, true};
+    int nExits = 0;
+    for (int i = 0; i < NUMBER_OF_LANES; i++)
+        if (isExit[i]) nExits++;
+
+    switch (nExits) {
+    case 1:
+        if (rand() % 2 == 0) {
+            // Place one block on non-exit
+            int i;
+            do i = rand() % 3;
+            while (isExit[i]);
+            val[i] = true;
+        } else {
+            // Place two blocks on non-exits
+            for (int i = 0; i < NUMBER_OF_LANES; i++) {
+                if (!isExit[i]) {
+                    val[i] = true;
+                }
+            }
+        }
+        break;
+    case 2:
+        if (isExit[1]) {
+            // Adjacent exits
+            if (rand() % 2 == 0) {
+                // Place one block randomly
+                val[rand() % 3] = true;
+            } else {
+                // Place block on the non-exit
+                for (int i = 0; i < NUMBER_OF_LANES; i++)
+                    if (!isExit[i]) val[i] = true;
+                // Place block on one exit randomly
+                int i;
+                do i = rand() % 3;
+                while (!isExit[i]);
+            }
+        } else {
+            // Nonadjacent exits
+            if (rand() % 2 == 0) {
+                val[1] = true;
+            }
+        }
+        break;
+    case 3:
+        if (rand() % 2 == 0) {
+            // Place one block randomly
+            val[rand() % 3] = true;
+        } else {
+            // Place two blocks randomly
+            int i = rand() % 3;
+            int j;
+            do j = rand() % 3;
+            while (j == i);
+            val[i] = true;
+            val[j] = true;
+        }
+        break;
+    default:
+        break;
+    }
+
+    // Update isExit array for next invocation
+    for (int i = 0; i < NUMBER_OF_LANES; i++) {
+        if (val[i]) {
+            isExit[i] = false;
+        } else {
+            isExit[i] = true;
+        }
+    }
+}
+
 tile* getTile() {
     tile *t = malloc(sizeof(tile));
-//    assert(t != NULL);
     t->next = NULL;
     t->prev = NULL;
     for (int i = 0; i < NUMBER_OF_LANES; i++) {
         t->value[i] = false;
     }
 
-    // Basic initialization for now
-    // Implement more complex formula later
-    t->value[0] = true;
+    setTileValue(t->value);
 
     return t;
 }
 
 tile *trackCreate() {
+    srand((unsigned)time(NULL));
     return NULL;
 }
 
