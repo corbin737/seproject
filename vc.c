4882ba97ba9147bde18596e65110c17395a60ebd
Corbin McElhanney <corbin.mcelhanney@gmail.com>
2016-11-17 14:01:26 -0500

    Replace delays with millis comparisons

--- vc.c
+++ vc.c
@@ -1,104 +1,117 @@
 #include "vc.h"
 
-static enum View
-{
+static enum View {
   Start = 0,
   Game = 1,
   GameOver = 2
-} vcView = Start;
+} vcView;
+typedef enum View View;
 
+const int drawDelay = 50;
+const int defaultGameTickDelay = 30;
+int tickDelay;
+
+int lastDrawTime, lastTickTime;
 tile *head;
 tile *onscreen;
 int offset;
 int lane;
-int fpks;
 int gameOverHeight;
 
+void setView(View view);
+
 void vcInit() {
-  startInit();
+  setView(Start);
 }
 
 void vcLoop(HardwareState state) {
-  int leftBtnState = state.leftBtn;
-  int rightBtnState = state.rightBtn;
-  int bottomSwitchState = state.bottomSwitch;
-  int topSwitchState = state.topSwitch;
-  switch(vcView) {
-  case Start:
-    trackLoopWithPushFunc(trackPushFullTile);
-    fpks = 8000;
-    startLoop(leftBtnState, rightBtnState);
-    break;
-  case Game:
-    if (bottomSwitchState == HIGH) break;
-    trackLoopWithPushFunc(trackPushRandTile);
-    if (checkCollision(lane) == 1) {
-      vcView = GameOver;
-      gameOverInit();
+  int currentTime = millis();
+  if (currentTime - lastTickTime > tickDelay) {
+    lastTickTime = currentTime;
+    switch(vcView) {
+    case Start:
+      trackTick(trackPushFullTile);
+      startTick(state.leftBtn, state.rightBtn);
+      break;
+    case Game:
+      if (state.bottomSwitch == HIGH) break;
+      if (state.topSwitch == HIGH) {
+        tickDelay = 10;
+      } else {
+        tickDelay = defaultGameTickDelay;
+      }
+      trackTick(trackPushRandTile);
+      carTick(state.leftBtn, state.rightBtn);
+      break;
+    case GameOver:
+      gameOverTick();
+      break;
     }
-    fpks += 50;
-    if (topSwitchState == HIGH) fpks += 100;
-    carLoop(leftBtnState, rightBtnState);
-    break;
-  case GameOver:
-    gameOverLoop();
-    fpks = 50000;
-    break;
   }
-
-  delay(1000000 / fpks);
-}
-
-void startInit() {
-  head = trackCreate();
-  for (int i = 0; i < 7; i++) {
-      head = trackPushFullTile(head);
-  }
-  onscreen = head->next;
-  offset = 0;
-}
-
-void startLoop(int leftBtnState, int rightBtnState) {
-  int bannerWidth = 64;
-
-  if (leftBtnState == HIGH || rightBtnState == HIGH) {
-    vcView = Game;
-    head = trackDelete(head);
-    trackInit();
-  } else {
-    OrbitOledSetFillPattern(OrbitOledGetStdPattern(iptnBlank));
-    OrbitOledSetDrawMode(modOledAnd);
-    OrbitOledMoveTo((SCREEN_HEIGHT / 2) - (bannerWidth / 2), 0);
-    OrbitOledFillRect((SCREEN_HEIGHT / 2) + (bannerWidth / 2), SCREEN_WIDTH);
-
-    OrbitOledSetFillPattern(OrbitOledGetStdPattern(iptnSolid));
-    OrbitOledSetDrawMode(modOledSet);
-    OrbitOledMoveTo((SCREEN_HEIGHT / 2) - (bannerWidth / 2), 0);
-    OrbitOledDrawString("Press");
-
-    OrbitOledMoveTo((SCREEN_HEIGHT / 2) - (bannerWidth / 2), SCREEN_WIDTH * 1 / 3);
-    OrbitOledDrawString("Button");
-
-    OrbitOledMoveTo((SCREEN_HEIGHT / 2) - (bannerWidth / 2), SCREEN_WIDTH * 2 / 3);
-    OrbitOledDrawString("to Begin");
-
+  if (currentTime - lastDrawTime > drawDelay) {
+    lastDrawTime = currentTime;
+    switch(vcView) {
+    case Start:
+      OrbitOledClear();
+      drawTrack(onscreen, offset);
+      drawStart();
+      break;
+    case Game:
+      if (state.bottomSwitch == HIGH) break;
+      OrbitOledClear();
+      drawTrack(onscreen, offset);
+      if (checkCollision(lane) == 1) {
+        setView(GameOver);
+      }
+      drawCar(lane);
+      break;
+    case GameOver:
+      drawGameOver(gameOverHeight);
+    }
     OrbitOledUpdate();
   }
 }
 
-void trackInit() {
-  // Populate first tiles visible on screen
+void startInit() {
+  if (head != NULL) {
+    head = trackDelete(head);
+  }
   head = trackCreate();
   for (int i = 0; i < 7; i++) {
-      head = trackPushBlankTile(head);
+    head = trackPushFullTile(head);
   }
   onscreen = head->next;
   offset = 0;
-  fpks = 40000;
+  tickDelay = 100;
 }
 
-void trackLoopWithPushFunc(tile *(*pushTile)(tile *)) {
+void gameInit() {
+  if (head != NULL) {
+    head = trackDelete(head);
+  }
+  head = trackCreate();
+  for (int i = 0; i < 7; i++) {
+    head = trackPushBlankTile(head);
+  }
+  onscreen = head->next;
+  offset = 0;
+  tickDelay = defaultGameTickDelay;
+}
+
+void gameOverInit() {
+  gameOverHeight = 0;
+  tickDelay = 10;
+}
+
+void startTick(int leftBtnState, int rightBtnState) {
+  if (leftBtnState == HIGH || rightBtnState == HIGH) {
+    setView(Game);
+  }
+}
+
+void trackTick(tile *(*pushTile)(tile *)) {
   // Update track model
+  offset++;
   if (offset >= 21) {
     // Bottom tile has moved off screen entirely
     offset %= 21;
@@ -109,41 +122,56 @@ void trackLoopWithPushFunc(tile *(*pushTile)(tile *)) {
     // Next tile has moved in on top of screen
     onscreen = onscreen->prev;
   }
-
-  OrbitOledClear();
-  OrbitOledSetFillPattern(OrbitOledGetStdPattern(iptnSolid));
-  OrbitOledSetDrawMode(modOledSet);
-  drawTrack(onscreen, offset);
-  offset++;
 }
 
-void trackLoop() {
-  trackLoopWithPushFunc(trackPushRandTile);
-}
-
-void carLoop(int leftBtnState, int rightBtnState) {
+void carTick(int leftBtnState, int rightBtnState) {
   lane = updateCarLane(leftBtnState, rightBtnState);
-  OrbitOledSetFillPattern(OrbitOledGetStdPattern(iptnSolid));
-  OrbitOledSetDrawMode(modOledSet);
-  drawCar(lane);
 }
 
-void gameOverInit() {
-  gameOverHeight = 0;
-}
-
-void gameOverLoop() {
+void gameOverTick() {
   if (gameOverHeight > SCREEN_HEIGHT) {
-    startInit();
-    vcView = Start;
-    delay(3000);
+    setView(Start);
+    delay(2000);
   }
-
-  OrbitOledSetFillPattern(OrbitOledGetStdPattern(iptnSolid));
-  OrbitOledSetDrawMode(modOledSet);
-  OrbitOledMoveTo(SCREEN_HEIGHT - gameOverHeight, 0);
-  OrbitOledLineTo(SCREEN_HEIGHT - gameOverHeight, SCREEN_HEIGHT);
-  OrbitOledUpdate();
   gameOverHeight++;
 }
 
+void drawGameOver(int height) {
+  OrbitOledSetFillPattern(OrbitOledGetStdPattern(iptnSolid));
+  OrbitOledSetDrawMode(modOledSet);
+  OrbitOledMoveTo(SCREEN_HEIGHT, 0);
+  OrbitOledFillRect(SCREEN_HEIGHT - gameOverHeight, SCREEN_WIDTH);
+}
+
+void drawStart() {
+  int bannerWidth = 64;
+
+  OrbitOledSetFillPattern(OrbitOledGetStdPattern(iptnBlank));
+  OrbitOledSetDrawMode(modOledAnd);
+  OrbitOledMoveTo((SCREEN_HEIGHT / 2) - (bannerWidth / 2), 0);
+  OrbitOledFillRect((SCREEN_HEIGHT / 2) + (bannerWidth / 2), SCREEN_WIDTH);
+
+  OrbitOledSetFillPattern(OrbitOledGetStdPattern(iptnSolid));
+  OrbitOledSetDrawMode(modOledSet);
+  OrbitOledMoveTo((SCREEN_HEIGHT / 2) - (bannerWidth / 2), 0);
+  OrbitOledDrawString("Press");
+  OrbitOledMoveTo((SCREEN_HEIGHT / 2) - (bannerWidth / 2), SCREEN_WIDTH * 1 / 3);
+  OrbitOledDrawString("Button");
+  OrbitOledMoveTo((SCREEN_HEIGHT / 2) - (bannerWidth / 2), SCREEN_WIDTH * 2 / 3);
+  OrbitOledDrawString("to Begin");
+}
+
+void setView(View view) {
+  vcView = view;
+  switch(vcView) {
+  case Start:
+    startInit();
+    break;
+  case Game:
+    gameInit();
+    break;
+  case GameOver:
+    gameOverInit();
+    break;
+  }
+}
